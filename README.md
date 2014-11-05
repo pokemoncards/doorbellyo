276db4506a696c5270dfbbe643b117497ad2fa83
Lucas Serven <lserven@gmail.com>
2014-11-05 00:43:59 -0500

    FIRST COMMIT

---
+++ README.md
@@ -0,0 +1,38 @@
+# yos from your doorbell wassup
+
+## installation
+
+    git clone git@github.com:pokemoncards/doorbellyo.git
+    cd doorbellyo
+    mkvirtualenv doorbellyo
+    pip install -r requirements.txt
+    
+install ngrok to receive requests from the outside world.
+    
+    brew install ngrok
+
+or
+
+    sudo apt-get install ngrok
+
+## usage
+start the service:
+
+    python app.py
+
+in another tab:
+
+    ngrok 5000
+
+## api
+### /
+your home screen with a subscriber count.
+
+### /count
+a subscriber count
+
+### /yo/username
+send a yo to `username`
+
+### /yoall
+send a yo to all your subscribers
---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, request
+import requests
+import config
+
+app = Flask(__name__)
+YO_API_KEY = config.YO_API_TOKEN
+
+@app.route('/')
+def index():
+    count_number = count()
+    return "smello, playa! looks like you have {0} subscribers".format(count_number)
+
+@app.route('/count')
+def count():
+    get_count = requests.get("https://api.justyo.co/subscribers_count/",
+                             params={'api_token': YO_API_KEY})
+
+    if 'result' in get_count.json() and type(get_count.json()['result']) is int:
+        return str(get_count.json()['result'])
+    return ":P"
+
+@app.route('/yo/<username>')
+def yo(username):
+    yo_request = requests.post("https://api.justyo.co/yo/",
+                                data={'api_token': YO_API_KEY, 'username': username})
+    if 'result' in yo_request.json() and yo_request.json()['result'] == "OK":
+        return "sent yo to {0}!".format(username)
+    return "got a dumb error :("
+
+@app.route('/yoall')
+def yoall():
+    yoall_request = requests.post("https://api.justyo.co/yoall/",
+                                data={'api_token': YO_API_KEY})
+    if yoall_request.text is "{}":
+        return "sent yo to everyone!"
+    return "got a dumb error :("
+
+@app.route('/callback')
+def callback():
+    username = request.args.get('username', '')
+    print(username)
+    return "got a yo from {0}".format(username)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
